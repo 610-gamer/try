7876a05a3155f9c1a1e6adfe5dbf93a756b8ef2e
610-gamer <76429774+610-gamer@users.noreply.github.com>
2021-11-24 21:21:02 +0800

    new assets

---
+++ Assets/scripts/control.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+    public float speed = 5f;
+    public float force = 10f;
+    public float H;
+    public float V;
+    public Rigidbody2D player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        H = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        V = Input.GetAxis("Jump") * speed * Time.deltaTime * 2;
+        this.gameObject.transform.Translate(H, V, 0);
+    }
+
+    void FixedUpdate()
+    {
+        player.AddForce(new Vector3(0, -force, 0));
+    }
+}
---
+++ Assets/scripts/dog_movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dog_movement: MonoBehaviour
+{
+
+    public float speed = 5f;
+    public float H;
+    public float V;
+    public Rigidbody2D dog;
+
+    void Start()
+    {
+        dog = GetComponent<Rigidbody2D>();
+        //this.gameObject.transform.Translate(a.position);
+    }
+
+    void Update()
+    {
+        H = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        V = Input.GetAxis("Jump") * speed * Time.deltaTime * 2;
+        this.gameObject.transform.Translate(-H,V, 0);
+    }
+
+    void FixedUpdate()
+    {
+            dog.AddForce(new Vector3(0, 20f, 0));
+    }
+
+}
+
